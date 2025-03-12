fb153a3f3883b6991b1cfaa830c7f40d3c8d7b71
Khushi Raghuvanshi <khraghuvanshi13@gmail.com>
2025-03-11 18:07:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-// Load data
-d3.json("data.json").then(function(data) {
-    
-    // Set SVG dimensions
-    const width = 800, height = 600;
-    const svg = d3.select("svg");
+const svg = d3.select("svg");
+const width = +svg.attr("width");
+const height = +svg.attr("height");
 
+// Load the dataset
+d3.json("data.json").then(data => {
     // Scale functions
-    const xScale = d3.scaleLinear()
-        .domain(d3.extent(data, d => d.Carbs))
-        .range([50, width - 50]);
+    const xScale = d3.scaleLinear().domain([0, 150]).range([50, width - 50]);
+    const yScale = d3.scaleLinear().domain([0, 50]).range([height - 50, 50]);
 
-    const yScale = d3.scaleLinear()
-        .domain(d3.extent(data, d => d.Protein))
-        .range([height - 50, 50]);
+    // Color scale for clusters
+    const colorScale = d3.scaleThreshold()
+        .domain([10, 25]) // Pre-diabetic, Diabetic
+        .range(["green", "orange", "red"]); // Healthy, Pre-diabetic, Diabetic
 
-    // Color scale
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-
-    // Create circles
-    svg.selectAll("circle")
+    // Draw initial circles
+    let circles = svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", d => xScale(d.Carbs))
-        .attr("cy", d => yScale(d.Protein))
+        .attr("cx", d => xScale(d.carbs))
+        .attr("cy", d => yScale(d.fat))
         .attr("r", 8)
-        .attr("fill", d => color(d.subject))
-        .attr("opacity", 0.7)
-        .on("mouseover", function(event, d) {
-            d3.select(this).attr("r", 12);
-        })
-        .on("mouseout", function(event, d) {
-            d3.select(this).attr("r", 8);
-        });
+        .attr("fill", d => colorScale(d.insulin))
+        .style("opacity", 0.8);
 
-    // Add axes
-    svg.append("g")
-        .attr("transform", `translate(0, ${height - 50})`)
-        .call(d3.axisBottom(xScale));
+    // Function to add user input data
+    window.addUserData = function () {
+        let carbs = +document.getElementById("carbs").value;
+        let protein = +document.getElementById("protein").value;
+        let fat = +document.getElementById("fat").value;
+        let fiber = +document.getElementById("fiber").value;
 
-    svg.append("g")
-        .attr("transform", `translate(50, 0)`)
-        .call(d3.axisLeft(yScale));
+        // Compute insulin prediction (Example formula, adjust for real data)
+        let insulinLevel = (carbs * 0.2) + (protein * 0.1) + (fat * 0.15) - (fiber * 0.05);
 
+        let classification = "Healthy";
+        if (insulinLevel > 25) classification = "Diabetic";
+        else if (insulinLevel > 10) classification = "Pre-diabetic";
+
+        // Add animated user data point
+        let newCircle = svg.append("circle")
+            .attr("cx", width / 2) // Start from center
+            .attr("cy", height / 2)
+            .attr("r", 12)
+            .attr("fill", colorScale(insulinLevel))
+            .style("opacity", 1);
+
+        newCircle.transition()
+            .duration(1000)
+            .attr("cx", xScale(carbs))
+            .attr("cy", yScale(fat))
+            .attr("r", 8);
+
+        alert(`You are classified as: ${classification} (Predicted Insulin: ${insulinLevel.toFixed(2)})`);
+    };
 });
